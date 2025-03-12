84843c1eb7310027d2681144e818dbc3305b11f5
Maksims Gnedovs <166118392+23DP1MGned@users.noreply.github.com>
2025-03-12 10:49:10 +0000

    transactions view added

--- app/models/Transactions.py
+++ app/models/Transactions.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime
+import os
 
 class Transactions:
     def __init__(self, transactions_type, amount, from_crypto, to_crypto):
@@ -18,17 +19,26 @@ class Transactions:
             "date": self.date
         }
 
-    @staticmethod
+
     def save_transactions(transaction):
         filename = "user_transactions.json"
-        
         try:
             with open(filename, "r") as file:
                 data = json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             data = {"transactions": []}
-
         data["transactions"].append(transaction.to_dict())
-
         with open(filename, "w") as file:
             json.dump(data, file, indent=4)
+
+    def view_transactions():
+        filename = "user_transactions.json"
+        with open(filename, "r") as file:
+            data = json.load(file)
+            transactions = data.get("transactions", [])
+                 
+            if transactions:
+                print("Transactions History:")
+                for txn in transactions:
+                    print(f"Date: {txn['date']} | Type: {txn['type']} | From: {txn['from']} | To: {txn['to']} | Amount: {txn['amount']}")
+        input("Press Enter to go back to the menu...")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 from app.models.CryptoWallet import CryptoWallet
+from app.models.Transactions import Transactions
 
 def clear_console():
     os.system("cls" if os.name == "nt" else "clear")
@@ -10,28 +11,36 @@ def main():
     
     while True:
         clear_console()
-        print("Current balance:")
         wallet.total_balance()
         print("Select an action: ")
         print("1 - Top up your balance in USDT")
         print("2 - Convert USDT to Crypto")
         print("3 - Convert Crypto to USDT")
-        print("4 - Exit")
+        print("4 - View transactions")
+        print("5 - Exit")
         
         choice = input("Enter action number: ")
         
         if choice == "1":
+            clear_console()
             amount = float(input("Enter the amount in USDT: "))
             wallet.top_up(amount)
         elif choice == "2":
+            clear_console()
+            print(f"Your balance: {wallet.balances['USDT']} USDT")
             crypto = input("Enter Crypto (BTC, ETH, SOL, DOT, TON, DOGE, LTC, XRP, ADA, AVAX): ").upper()
             amount = float(input("Enter the amount in USDT to convert: "))
             wallet.usdt_to_crypto(crypto, amount)
         elif choice == "3":
+            clear_console()
             crypto = input("Enter Crypto to convert to USDT (BTC, ETH, SOL, DOT, TON, DOGE, LTC, XRP, ADA, AVAX): ").upper()
             amount = float(input("Enter the amount to convert: "))
             wallet.crypto_to_usdt(crypto, amount)
         elif choice == "4":
+            clear_console()
+            Transactions.view_transactions()
+        elif choice == "5":
+            clear_console()
             print("Exit...")
             wallet.save_wallet()
             break
@@ -39,6 +48,7 @@ def main():
             print("Invalid input, please try again.")
         
         time.sleep(2)
+
 
 if __name__ == "__main__":
     main()
